361bbc7b66e78468698b62aed3d191fc3dd2e04b
Victor <vlcampos921@gmail.com>
2022-08-29 13:14:09 -0300

    feat: Create partial documentation

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import "express-async-errors";
 import express from "express";
-import { useRoutes } from "./routes";
 import { handleErrors } from "./errors";
+import SwaggerUI from "swagger-ui-express";
+import swaggerDoc from "../swagger-output.json";
+import { useRoutes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/doc", SwaggerUI.serve, SwaggerUI.setup(swaggerDoc));
 
 useRoutes(app);
 
